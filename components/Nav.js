7ed66ce6f484a62deaaeb303bf949a985c4985b2
David McDonald <davidsmcdonald@gmail.com>
2021-02-17 12:10:04 -0800

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import SignOut from './SignOut';
+import NavStyles from './styles/NavStyles';
+import { useUser } from './User';
+
+export default function Nav() {
+  const user = useUser();
+  return (
+    <NavStyles>
+      <Link href="/products">Wines</Link>
+      {user && (
+        <>
+          <Link href="/admin">Admin</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+          <SignOut />
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
+    </NavStyles>
+  );
+}
---
+++ components/Product.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+import DeleteProduct from './DeleteProduct';
+
+export default function Product({ product }) {
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <p>
+        <Link href={`product/${product.id}`}>{product.name}</Link>
+      </p>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <div className="buttonList">
+        <Link 
+          href={{
+            pathname: 'update',
+            query: {
+            id: product.id,
+            },
+          }}
+        >
+          Edit ✎
+        </Link>
+        <DeleteProduct id={product.id}>Delete</DeleteProduct>
+      </div>
+    </ItemStyles>
+  );
+}
